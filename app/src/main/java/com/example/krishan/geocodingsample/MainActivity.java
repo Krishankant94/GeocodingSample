168d1a28877cd0a53e154d499801db5bee121d3f
Krishan Kant <kumkum.sharma@metlife.com>
2018-02-19 17:05:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/krishan/geocodingsample/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.krishan.geocodingsample;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    TextView currentCity;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        currentCity = (TextView) findViewById(R.id.own_text);
+        getAddressFromLocation(this, new GeoCoderHandler());
+    }
+    public static void getAddressFromLocation(final Context context, final Handler handler) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+                String result = null;
+                try {
+                    List<Address> list = geocoder.getFromLocation(26.9124, 75.7873, 1);
+                    if (list != null && list.size() > 0) {
+                        Address address = list.get(0);
+                        // sending back first address line and locality
+                        result = address.getAddressLine(0) + ", " + address.getLocality() + ", " + address.getCountryName();
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Impossible to connect to Geocoder", e);
+                } finally {
+                    Message msg = Message.obtain();
+                    msg.setTarget(handler);
+                    if (result != null) {
+                        msg.what = 1;
+                        Bundle bundle = new Bundle();
+                        bundle.putString("address", result);
+                        msg.setData(bundle);
+                    } else
+                        msg.what = 0;
+                    msg.sendToTarget();
+                }
+            }
+        };
+        thread.start();
+    }
+     class GeoCoderHandler extends Handler {
+        @Override
+        public void handleMessage(Message message) {
+            String result;
+            switch (message.what) {
+                case 1:
+                    Bundle bundle = message.getData();
+                    result = bundle.getString("address");
+                    break;
+                default:
+                    result = null;
+            }
+            currentCity.setText(result);
+        }
+    }
+
+}
